2bd104cc65611088a4c5daf01a3b2b5453672a00
chienduynguyen1702 <chiennd172002@gmail.com>
2024-03-18 03:00:44 +0700

    added params data

--- src/hooks/mocks/parameters.js
+++ src/hooks/mocks/parameters.js
@@ -50,4 +50,56 @@ export const PARAMETERS = [
     created_at: '2024-03-02T10:00:00Z',
     updated_at: '2024-03-02T10:00:00Z',
   },
+  {
+    id: 4,
+    name: 'API_KEY',
+    value: '1234567890',
+    stage: {
+      id: 1,
+      name: 'Build',
+      color: '#B5E4CA',
+    },
+    environment: {
+      id: 1,
+      name: 'Dev/Test',
+      color: '#659EEA',
+    },
+    created_at: '2024-03-02T10:00:00Z',
+    updated_at: '2024-03-02T10:00:00Z',
+  },
+  {
+    id: 5,
+    name: 'API_KEY',
+    value: '1234567890',
+    stage: {
+      id: 1,
+      name: 'Build',
+      color: '#B5E4CA',
+    },
+    environment: {
+      id: 2,
+      name: 'Staging',
+      color: '#F6D55C',
+    },
+    created_at: '2024-03-02T10:00:00Z',
+    updated_at: '2024-03-02T10:00:00Z',
+  },
+  {
+    id: 6,
+    name: 'API_KEY',
+
+    value: '1234567890',
+    stage: {
+      id: 1,
+      name: 'Build',
+      color: '#B5E4CA',
+    },
+    environment: {
+      id: 3,
+      name: 'Production',
+      color: '#FFAFF2',
+    },
+    created_at: '2024-03-02T10:00:00Z',
+    updated_at: '2024-03-02T10:00:00Z',
+  },
 ];
