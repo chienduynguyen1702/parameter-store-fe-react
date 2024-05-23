e949196fa510a8e7071249c244cce22a1aef1e4d
chiennd172002 <chiennd172002@gmail.com>
2024-05-24 00:43:27 +0700

    remove commented code

--- src/hooks/data/organizations/useOrganizationDashboardTotal.js
+++ src/hooks/data/organizations/useOrganizationDashboardTotal.js
@@ -4,62 +4,8 @@ import { getOrganizationDashboardTotals } from '../../../services/api';
 
 const useOrganizationDashboard = (organizationId, project, from, to) => {
   const parseData = (data) => {
-    // console.log('useOrganizationDashboard data', data);
-    //   [
-    //     {
-    //         "value": 6,
-    //         "name": "project_count",
-    //         "label": "Projects Count"
-    //     },
-    //     {
-    //         "value": 2,
-    //         "name": "active_projects_count",
-    //         "label": "Active Projects Count"
-    //     },
-    //     {
-    //         "value": 4,
-    //         "name": "active_agent",
-    //         "label": "Active Agent"
-    //     },
-    //     {
-    //         "value": 4,
-    //         "name": "user_count",
-    //         "label": "User Count"
-    //     },
-    //     {
-    //         "value": 4,
-    //         "name": "workflow_count",
-    //         "label": "Workflow Count"
-    //     },
-    //     {
-    //         "value": 122000,
-    //         "name": "avg_duration",
-    //         "label": "Average Duration"
-    //     },
-    //     {
-    //         "value": 118,
-    //         "name": "total_updated",
-    //         "label": "Total Updated"
-    //     },
-    //     {
-    //         "value": 118,
-    //         "name": "total_updated_this_month",
-    //         "label": "Total Updated This Month"
-    //     },
-    //     {
-    //         "value": 73,
-    //         "name": "total_agent_actions",
-    //         "label": "Total Agent Actions"
-    //     },
-    //     {
-    //         "value": 73,
-    //         "name": "total_agent_actions_this_month",
-    //         "label": "Total Agent Actions This Month"
-    //     }
-    // ]
     const total = data;
 
-    // console.log('useOrganizationDashboard total', total);
     return { total };
   };
 
