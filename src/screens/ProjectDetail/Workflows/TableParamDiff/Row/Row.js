2b925b332d1a8c58be8d10130a2baf4b2a44bee1
chiennd172002 <chiennd172002@gmail.com>
2024-07-07 01:09:10 +0700

    update color when add, remove, change parameter

--- src/screens/ProjectDetail/Workflows/TableParamDiff/Row/Row.js
+++ src/screens/ProjectDetail/Workflows/TableParamDiff/Row/Row.js
@@ -2,19 +2,39 @@ import React from 'react';
 
 const Row = ({ item }) => {
   // console.log('item', item);
+  let defaultPreviousParamColor = '#f5f5f5';
+  let defaultCurrentParamColor = '#f5f5f5';
+  if (item.current_value === '') {
+    // Add a condition to check if the current value is null, meaning it has been deleted
+    defaultPreviousParamColor = 'red';
+  } else if (item.previous_value === '') {
+    // Add a condition to check if the previous value is null, meaning it has been added
+    defaultCurrentParamColor = 'green';
+  } else if (item.current_value !== item.previous_value) {
+    // Add a condition to check if the current value is different from the previous value
+    defaultCurrentParamColor = 'yellow';
+  }
 
   return (
     <div className="tableRow">
       <div className="tableCell py-3 ps-2 roundedLeft">
         <p>{item.name}</p>
       </div>
-      <p className="tableCell">{item.previous_value}</p>
       <div className="tableCell roundedRight">
         <p
           className="status-default"
           style={{
-            backgroundColor:
-              item.current_value !== item.previous_value ? 'yellow' : '#f5f5f5',
+            backgroundColor: defaultPreviousParamColor,
+          }}
+        >
+          {item.previous_value}
+        </p>
+      </div>
+      <div className="tableCell roundedRight">
+        <p
+          className="status-default"
+          style={{
+            backgroundColor: defaultCurrentParamColor,
           }}
         >
           {item.current_value}
